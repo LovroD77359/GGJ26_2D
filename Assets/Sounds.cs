9d475d87f9c893600768afe395ea6f395ee63df2
NiksaKepecc <niksaniksa10@gmail.com>
2026-02-01 03:13:25 +0100

    Sounds
    sounds skripta i sfx uz animaciju

---
+++ Assets/Sounds.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Sounds : MonoBehaviour
+{
+    private AudioSource audioSource;
+    bool looping = false;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    public void PlayLoopingSFX(AudioClip clip)
+    {
+        if (looping && clip == audioSource.clip)
+            return;
+        audioSource.clip = clip;
+        audioSource.Play();
+        audioSource.loop = true;
+        looping = true;
+    }
+
+    public void StopSFX(AudioClip clip)
+    {
+        if (clip != audioSource.clip)
+            return;
+        audioSource.Stop();
+        looping = false;
+    }
+
+}
