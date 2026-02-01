43559a95ac5090b39575536cb44f7fec4b04a793
NiksaKepecc <niksaniksa10@gmail.com>
2026-02-01 14:59:15 +0100

    Tutorial Buttons

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,7 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public GameObject tutorial;
+    public GameObject tutorial, ButtonNext, ButtonPrevious, ButtonOkay, Panel1, Panel2;
+    
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadSceneAsync(sceneName);
@@ -17,5 +18,24 @@ public class MainMenu : MonoBehaviour
     public void ToggleTutorial()
     {
         tutorial.SetActive(!tutorial.activeInHierarchy);
+        TutorialPreviousPanel();
     }
+
+    public void TutorialNextPanel()
+    {
+        Panel2.SetActive(true);
+        ButtonOkay.SetActive(true);
+        ButtonPrevious.SetActive(true);
+        Panel1.SetActive(false);
+        ButtonNext.SetActive(false);
+    }
+
+    public void TutorialPreviousPanel()
+    {
+        Panel1.SetActive(true);
+        ButtonNext.SetActive(true);
+        Panel2.SetActive(false);
+        ButtonPrevious.SetActive(false);
+    }
+
 }
